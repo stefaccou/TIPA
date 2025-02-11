3b5ec69cf65b138c48435e972b142a403e781d4a
Stef Accou <stef.accou@student.kuleuven.be>
2025-02-11 15:59:54 +0100

    corrections for VSC

--- lang-adapters-impact/train_sbatch.sh
+++ lang-adapters-impact/train_sbatch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#SBATCH -A NAISS2023-22-455 -p alvis
-#SBATCH -N 1 --gpus-per-node=A100:1
-#SBATCH -t 0-08:00:00
+#SBATCH --cluster=wice
+#SBATCH --partition=interactive
+#SBATCH --account=lp_hpcinfo
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --time=2:00
+#SBATCH --job-name=test_for_adapters
+#SBATCH --output=%x.out
 
 TASK=$1
 DATA_SETUP=$2
@@ -11,11 +15,11 @@ NONE_TR=$5
 SEED=$6
 EPOCHS=$7
 
-mkdir ${TMPDIR}/train_res
-mkdir ${TMPDIR}/eval_res
+mkdir ${VSC_DATA}/train_res
+mkdir ${VSC_DATA}/eval_res
 
-TRAIN_OUTPUT_DIR="${TMPDIR}/train_res/${DATA_SETUP}_${TASK}_${TRAIN_LANG}_${BASE_MODEL}_NONE_TR=${NONE_TR}_SEED=${SEED}_EPOCHS=${EPOCHS}_DATE=$(date +"%Y-%m-%d_%H:%M")"
-EVAL_OUTPUT_DIR="${TMPDIR}/eval_res/${TASK}_${DATA_SETUP}_${TRAIN_LANG}_${BASE_MODEL}_NONE_TR=${NONE_TR}_SEED=${SEED}_EPOCHS=${EPOCHS}_DATE=$(date +"%Y-%m-%d_%H:%M")"
+TRAIN_OUTPUT_DIR="${VSC_DATA}/train_res/${DATA_SETUP}_${TASK}_${TRAIN_LANG}_${BASE_MODEL}_NONE_TR=${NONE_TR}_SEED=${SEED}_EPOCHS=${EPOCHS}_DATE=$(date +"%Y-%m-%d_%H:%M")"
+EVAL_OUTPUT_DIR="${VSC_DATA}/eval_res/${TASK}_${DATA_SETUP}_${TRAIN_LANG}_${BASE_MODEL}_NONE_TR=${NONE_TR}_SEED=${SEED}_EPOCHS=${EPOCHS}_DATE=$(date +"%Y-%m-%d_%H:%M")"
 echo "TRAIN_OUTPUT_DIR: $TRAIN_OUTPUT_DIR"
 echo "EVAL_OUTPUT_DIR: $EVAL_OUTPUT_DIR"
 
