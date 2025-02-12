07ad750a503d318b98cf3195be147bed25d57246
Stef Accou <stef.accou@student.kuleuven.be>
2025-02-12 13:21:45 +0100

    new day new tests

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
-# Test file to print hello world and save it to a text file
-print("Hello World")
+import submitit
 
-# Save the output to a text file
-with open("output.txt", "w") as file:
-    file.write("Hello World\n")
+
+def add(a, b):
+    return a + b
+
+
+# executor is the submission interface (logs are dumped in the folder)
+executor = submitit.AutoExecutor(folder="log_test")
+# set timeout in min, and partition for running the job
+executor.update_parameters(timeout_min=1, slurm_partition="debug-a100")
+job = executor.submit(add, 5, 7)  # will compute add(5, 7)
+print(job.job_id)  # ID of your job
+
+output = job.result()  # waits for completion and returns output
+assert output == 12  # 5 + 7 = 12...  your addition was computed in the cluster
