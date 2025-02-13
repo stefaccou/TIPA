054f5193864f9a41a5cf74bb250332fa7dbd0f31
Stef Accou <stef.accou@student.kuleuven.be>
2025-02-13 13:39:14 +0100

    new_account

--- hello.py
+++ hello.py
@@ -1,35 +1,32 @@
 import submitit
-import os
-
-os.environ["SLURM_JOB_ID"] = "thisIsATest"
-os.environ["SLURM_CLUSTER_NAME"] = "wice"
 
 
-def add(a, b):
+def expensive_function(a, b):
     return a + b
 
 
-# SBATCH --job-name=test_for_slurm_and_stuff
-# SBATCH --output=%x.out
-
-# executor is the submission interface (logs are dumped in the folder)
-executor = submitit.AutoExecutor(folder="log_test")
-# set timeout in min, and partition for running the job
-executor.update_parameters(
-    timeout_min=1,
-    slurm_partition="debug-a100",
-    slurm_additional_parameters={
-        "job_name": "Submitit_test",
+# Either define 'manually' or read in from yaml or Hydra:
+parameters = {
+    "slurm_partition": "gpu_a100_debug",
+    "slurm_time": "00:10:00",
+    "slurm_job_name": "<experiment-id>",  # Put something informative here, it helps tracking down issues easier
+    "slurm_additional_parameters": {
         "clusters": "wice",
-        "account": "intro_vsc37220",
+        "account": "intro_vsc37220",  # Your VSC account
         "nodes": 1,
         "cpus_per_gpu": 64,
         "gpus_per_node": 1,
-        "output": "/test_for_output",
+        "mail_type": "BEGIN,END,FAIL",
+        "mail_user": "",  # for notifications specified above
     },
-)
-job = executor.submit(add, 5, 7)  # will compute add(5, 7)
-print(job.job_id)  # ID of your job
+}
 
-output = job.result()  # waits for completion and returns output
-assert output == 12  # 5 + 7 = 12...  your addition was computed in the cluster
+# All logs and slurm files will be stored in this folder
+executor = submitit.AutoExecutor(folder="experiment_folder")
+executor.update_parameters(**parameters)
+
+job = executor.submit(expensive_function, 5, 7)
+# This waits for completion and returns output, often not a good idea with long jobs,
+# then you can just let this script finish.
+output = job.result()
+assert output == 12  # computed in the cluster
