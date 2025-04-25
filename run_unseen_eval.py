1fb86cab831e3096e9513fd92c9304760480db87
Stef Accou <stef.accou@student.kuleuven.be>
2025-04-25 18:44:48 +0200

    ner eval on unseen languages

---
+++ run_unseen_eval.py
@@ -0,0 +1,250 @@
+# This script will load in all existing adapters from the huggingface library and re-construct an unseen language adapter
+import submitit
+import os
+import sys
+from custom_submission_utils import find_master, update_submission_log
+
+def main(iterations=1):
+
+    from unseen_eval import get_available_adapters, merge_loaded_adapters, typological_approximation, get_glots
+
+    from transformers import TrainingArguments, AutoTokenizer
+    from adapters import AdapterTrainer, init, AutoAdapterModel
+    from adapters.composition import Stack
+
+    from datasets import load_dataset, get_dataset_config_names
+    from transformers import DataCollatorForTokenClassification, AutoModelForTokenClassification
+    import os
+    import re
+    import gc
+    import json
+    from collections import OrderedDict
+    import torch
+    import numpy as np
+    import evaluate
+    metric = evaluate.load("seqeval")
+    import random
+
+    from huggingface_hub import HfApi
+    api = HfApi()
+
+    from qq import LanguageData, TagType
+    ld = LanguageData.from_db()
+
+    from urielplus import urielplus
+    u = urielplus.URIELPlus()
+    u.set_cache(True)
+    try:
+        u.integrate_grambank()
+    except:
+        print("already using GramBank")
+    try:
+        u.set_glottocodes()
+    except:
+        print("already using Glottocodes")
+
+
+    eval_languages = get_dataset_config_names("wikiann")
+
+    tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
+    data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+
+
+
+    model = AutoAdapterModel.from_pretrained("xlm-roberta-base")
+
+    to_load = get_available_adapters()
+    for link, id in to_load.items():
+        try:
+            model.load_adapter(link, load_as=id)
+        except OSError:
+            print(f"Could not load {link}")
+            continue
+
+    for i in range(iterations):
+
+        eval_language = random.choice([lan for lan in eval_languages if len(lan) <= 3])
+        print(
+            f"Evaluating on randomly chosen language {eval_language} ({ld.get(eval_language, tag_type=TagType.BCP_47_CODE).english_name})")
+
+        dataset_eval = load_dataset("wikiann", eval_language, trust_remote_code=True)
+        # If True, all tokens of a word will be labeled, otherwise only the first token
+        label_all_tokens = True
+
+        def align_labels_with_tokens(labels, word_ids):
+            new_labels = []
+            current_word = None
+            for word_id in word_ids:
+                if word_id != current_word:
+                    # Start of a new word!
+                    current_word = word_id
+                    label = -100 if word_id is None else labels[word_id]
+                    new_labels.append(label)
+                elif word_id is None:
+                    # Special token
+                    new_labels.append(-100)
+                else:
+                    # Same word as previous token
+                    label = labels[word_id]
+                    # If the label is B-XXX we change it to I-XXX
+                    if label % 2 == 1:
+                        label += 1
+                    new_labels.append(label)
+
+            return new_labels
+
+        # Batch encoding function for NER
+        def tokenize_and_align_labels(examples):
+            tokenized_inputs = tokenizer(
+                examples["tokens"], truncation=True, is_split_into_words=True
+            )
+            all_labels = examples["ner_tags"]
+            new_labels = []
+            for i, labels in enumerate(all_labels):
+                word_ids = tokenized_inputs.word_ids(i)
+                new_labels.append(align_labels_with_tokens(labels, word_ids))
+
+            tokenized_inputs["labels"] = new_labels
+            return tokenized_inputs
+
+        # Load and preprocess WikiANN
+
+        tokenized_datasets = dataset_eval.map(
+            tokenize_and_align_labels,
+            batched=True,
+            remove_columns=dataset_eval["train"].column_names,
+        )
+
+        # dataset_en is now ready to be used with adapters for cross-lingual transfer
+
+        ner_feature = dataset_eval["train"].features["ner_tags"]
+
+        label_names = ner_feature.feature.names
+        words = dataset_eval["train"][0]["tokens"]
+        labels = dataset_eval["train"][0]["ner_tags"]
+        id2label = {i: label for i, label in enumerate(label_names)}
+        label2id = {v: k for k, v in id2label.items()}
+
+        def compute_metrics(eval_preds):
+            logits, labels = eval_preds
+            predictions = np.argmax(logits, axis=-1)
+
+            # Remove ignored index (special tokens) and convert to labels
+            true_labels = [[label_names[l] for l in label if l != -100] for label in labels]
+            true_predictions = [
+                [label_names[p] for (p, l) in zip(prediction, label) if l != -100]
+                for prediction, label in zip(predictions, labels)
+            ]
+            all_metrics = metric.compute(predictions=true_predictions, references=true_labels)
+            return {
+                "precision": all_metrics["overall_precision"],
+                "recall": all_metrics["overall_recall"],
+                "f1": all_metrics["overall_f1"],
+                "accuracy": all_metrics["overall_accuracy"],
+            }
+
+        def eval(model, name):
+            # we load in the task adapter
+            if not name == "ner":
+                model.active_adapters = Stack(name, "ner")
+            else:
+                model.active_adapters = name
+
+            eval_trainer = AdapterTrainer(
+                model=model,
+                args=TrainingArguments(output_dir="./eval_output", remove_unused_columns=False, ),
+                data_collator=data_collator,
+                eval_dataset=tokenized_datasets["validation"],
+                compute_metrics=compute_metrics,
+            )
+            ev = eval_trainer.evaluate()
+            print(f"Evaluation results for {name}:")
+            print(ev)
+            # we empty the cache and model
+            model.cpu()
+            del model
+            del eval_trainer
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+            gc.collect()
+
+            return ev
+
+
+
+
+        # we check if the adapter has already been created before
+        if os.path.exists(f"./trained_adapters/typological/{eval_language}"):
+            print("Adapter already exists, loading instead")
+            model.load_adapter(f"./trained_adapters/typological/{eval_language}", load_as="reconstructed_" + eval_language)
+        else:
+
+            target_glot = ld.get(eval_language, tag_type=TagType.BCP_47_CODE).glottocode
+            weights = typological_approximation(target_glot, get_glots(to_load))
+
+            merge_loaded_adapters(model, merge_adapter_name=f"reconstructed_{eval_language}", weights=weights,
+                                  delete_other=False)
+            # save this adapter
+            # check if directory exists first
+            if not os.path.exists(f"./trained_adapters/typological/{eval_language}"):
+                os.makedirs(f"./trained_adapters/typological/{eval_language}")
+            model.save_adapter(f"./trained_adapters/typological/{eval_language}", "reconstructed_" + eval_language)
+
+        model.load_adapter("./trained_adapters/ner", load_as="ner")
+
+        evaluations = {}
+        evaluations["reconstructed_" + eval_language] = eval(model, f"reconstructed_{eval_language}")
+
+        # we calculate a baseline (just ner adapter)
+        evaluations["baseline_ner"] = eval(model, "ner")
+
+        # we calculate a baseline (just average over all adapter)
+        # we load the mono/huge_avg_adapter for this
+        model.load_adapter("./trained_adapters/typological/huge_avg_adapter", load_as="huge_avg_adapter")
+        evaluations["baseline_avg_adapter"] = eval(model, "huge_avg_adapter")
+        # we delete the added adapters
+        model.delete_adapter("huge_avg_adapter")
+        model.delete_adapter("ner")
+        model.delete_adapter("reconstructed_" + eval_language)
+
+        # we save this
+        with open(f"./trained_adapters/typological/{eval_language}/eval.json", "w") as f:
+            json.dump(evaluations, f, indent=4)
+            print("Saved evaluations to file")
+
+
+if __name__ == "__main__":
+    job_name = "unseen_eval_random_debug"
+
+    master_dir = find_master()
+
+    # Set the experiment folder as a subdirectory of 'Master_thesis'
+    experiments_dir = master_dir / "experiment_folder"
+
+    run_count = update_submission_log(experiments_dir, job_name)
+    experiments_dir = experiments_dir / job_name / f"{run_count:03d}"
+    experiments_dir.mkdir(parents=True, exist_ok=True)  # Create if it doesn't exist
+    parameters = {
+        "slurm_partition": "gpu_a100_debug",
+        "slurm_time": "00:10:00",
+        "slurm_job_name": job_name,
+        "slurm_additional_parameters": {
+            "clusters": "wice",
+            "account": os.environ["ACCOUNT_INFO"],  # replace with your account
+            "nodes": 1,
+            "cpus_per_gpu": 16,
+            "gpus_per_node": 1,
+            "mail_type": "BEGIN,END,FAIL",
+            "mail_user": "",
+        },
+    }
+
+    # Initialize the Submitit executor with the new experiments_dir
+    executor = submitit.AutoExecutor(folder=str(experiments_dir))
+    executor.update_parameters(**parameters)
+
+    job_input = sys.argv[1:] if len(sys.argv) > 1 else "default text"
+
+    job = executor.submit(main, job_input)
+    #job = executor.submit(main)
+    print("job submitted")
---
+++ unseen_eval.py
@@ -0,0 +1,170 @@
+
+import re
+from collections import OrderedDict
+import torch
+import evaluate
+metric = evaluate.load("seqeval")
+
+
+from huggingface_hub import HfApi
+api = HfApi()
+
+from qq import LanguageData, TagType
+ld = LanguageData.from_db()
+
+from urielplus import urielplus
+u = urielplus.URIELPlus()
+u.set_cache(True)
+try:
+    u.integrate_grambank()
+except:
+    print("already using GramBank")
+try:
+    u.set_glottocodes()
+except:
+    print("already using Glottocodes")
+
+def get_available_adapters():
+    # get all adapters from huggingface
+    all_models = api.list_models(author="AdapterHub",
+                                 library="adapter-transformers",
+                                 search="xlm-roberta-base-"
+                                 )
+
+    to_load = {
+        m.modelId: m.modelId.split("xlm-roberta-base-")[-1].rsplit("-wiki_pfeiffer", 1)[0]
+        for m in all_models
+        if m.modelId.startswith("AdapterHub/xlm-roberta-base-")
+           and m.modelId.endswith("-wiki_pfeiffer")
+    }
+    return to_load
+
+def get_glots(to_load):
+    manuals = {"Arabic": "arab1267", "Swahili": "swah1253", "Bengali": "beng1282", "Chinese": "mand1415",
+               "Persian": "west2369", "Yoruba": "ilaa1246", "Nepali": "nepa1254", "Quechua": "cusc1236",
+               "Estonian": "esto1258", "Guarani": "east2555"}
+
+    glots = {}
+    probs = []
+
+    for lang in to_load.values():
+        eng = ld.get(lang, tag_type=TagType.BCP_47_CODE).english_name
+        glot = ld.get(lang, tag_type=TagType.BCP_47_CODE).glottocode
+        # we need to find if glot is in distances
+        if not glot:
+            if eng in manuals.keys():
+                glot = manuals[eng]
+        if eng and glot:
+            glots[lang] = glot
+        else:
+            probs.append(lang)
+
+    print("no glottocodes found for these languages: ", probs)
+    print("removing them from further consideration")
+    for prob in probs:
+        del to_load[prob]
+        # happens in-place
+    return glots
+
+def merge_loaded_adapters(model, merge_adapter_name="joined_adapter", weights=None, delete_other=False,
+                          patterns=False, model_type="roberta"):
+    # to ensure we don't get problems, we check the config of all adapters
+    all_adapters = list(model.adapters_config.adapters.keys())
+    config_id = model.adapters_config.adapters[all_adapters[0]]
+    config = model.adapters_config.config_map[config_id]
+
+    for i in range(1, len(all_adapters)):
+        config_id = model.adapters_config.adapters[all_adapters[i]]
+        config_i = model.adapters_config.config_map[config_id]
+        assert config == config_i, (f"Config mismatch: {config} vs {config_i}"
+                                    f"\nCurrent methodology only works for same config")
+
+    if weights == None:
+        weights = [1 / len(all_adapters)] * len(all_adapters)
+    if len(weights) != len(all_adapters):
+        raise ValueError(f"Weights length {len(weights)} does not match number of adapters {len(all_adapters)}")
+
+    if not patterns:
+        patterns = [f"{model_type}\.encoder\.layer\.([\d\w]+)\.output\.adapters\.(?:\w+)\.(\w+)(?:\.0)?\.(\w+)",
+                    f"{model_type}\.invertible_adapters\.(?:\w+)\.(\w+)\.(\d)\.(\w+)"]
+    comp_patterns = [re.compile(pattern) for pattern in patterns]
+    organized_layers = {}
+    for i, pattern in enumerate(patterns):
+        # we make a dictionary for each pattern
+        organized_layers[i] = {}
+
+    for key in model.state_dict().keys():
+        for i, pattern in enumerate(comp_patterns):
+            match = re.search(pattern, key)
+            if match:
+                one = match.group(1)
+                two = match.group(2)
+                three = match.group(3)
+                if one not in organized_layers[i]:
+                    organized_layers[i][one] = {}
+                if two not in organized_layers[i][one]:
+                    organized_layers[i][one][two] = {}
+                if three not in organized_layers[i][one][two]:
+                    organized_layers[i][one][two][three] = []
+                organized_layers[i][one][two][three].append(key)
+
+    new_state_dict = OrderedDict()
+    sd = model.state_dict()
+
+    for i, one in organized_layers.items():
+        for one, two in one.items():
+            for two, three in two.items():
+                for three, keys in three.items():
+                    result = sum([sd[key] * weights[j] for j, key in enumerate(keys)])
+                    if two == "adapter_down":
+                        new_state_dict[
+                            f"{model_type}.encoder.layer.{one}.output.adapters.{merge_adapter_name}.{two}.0.{three}"] = result
+                    elif two == "adapter_up":
+                        new_state_dict[
+                            f"{model_type}.encoder.layer.{one}.output.adapters.{merge_adapter_name}.{two}.{three}"] = result
+                    else:
+                        # we are in the second pattern
+                        new_state_dict[
+                            f"{model_type}.invertible_adapters.{merge_adapter_name}.{one}.{two}.{three}"] = result
+
+    # we now load in the new model
+    if merge_adapter_name in model.adapters_config.adapters.keys():
+        # remove the old one
+        model.delete_adapter(merge_adapter_name)
+    model.add_adapter(merge_adapter_name, config=config)
+    for name, param in model.named_parameters():
+        # e.g. "roberta.encoder.layer.0.output.adapters.joined_adapter.adapter_down.0.weight"
+        if merge_adapter_name in name and name in new_state_dict:
+            param.data.copy_(new_state_dict[name])
+    if delete_other:
+        for key in list(model.adapters_config.adapters.keys()):
+            if key != merge_adapter_name:
+                model.delete_adapter(key)
+
+    # no need to return anything as the model is changed in place
+
+def typological_approximation(target, glots):
+    """
+    This function takes a target language and a list of languages.
+    It weights the other languages depending on their closeness to the target language.
+    """
+
+    # 1. retrieve closeness score of all languages to target language
+    weights = []
+    for lang, glot in glots.items():
+        # get the distance
+        try:
+            dist = u.new_distance("featural", [glot, target])
+            print(f"Distance {lang} to {target}: {dist}")
+        except:
+            print(f"Error: {lang} - {glot} - {target}")
+            dist = 0
+        weights.append(dist)
+
+    # 1. softmax over weights
+    print(f"Weights before softmax: {weights}")
+    weights = torch.softmax(torch.tensor(weights), dim=0)
+    # we need to convert to list
+    weights = weights.tolist()
+    print(f"Weights after softmax: {weights}")
+
