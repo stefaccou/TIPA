74f3e416fa205dbec9a343b711a70d5478dda7b3
Stef Accou <stef.accou@student.kuleuven.be>
2025-04-26 09:56:19 +0200

    ner eval on unseen languages

--- run_unseen_eval.py
+++ run_unseen_eval.py
@@ -66,157 +66,163 @@ def main(job_input):
         print("No iterations given, using default of 1")
         iterations = 1
     for i in range(iterations):
-        eval_language = random.choice([lan for lan in eval_languages if len(lan) <= 3])
-        eval_languages.remove(eval_language)
-        print(
-            f"Evaluating on randomly chosen language {eval_language} ({ld.get(eval_language, tag_type=TagType.BCP_47_CODE).english_name})"
-        )
+        try:
+            eval_language = random.choice([lan for lan in eval_languages if len(lan) <= 3])
+            eval_languages.remove(eval_language)
+            print(
+                f"Evaluating on randomly chosen language {eval_language} ({ld.get(eval_language, tag_type=TagType.BCP_47_CODE).english_name})"
+            )
 
-        dataset_eval = load_dataset("wikiann", eval_language, trust_remote_code=True)
-        # If True, all tokens of a word will be labeled, otherwise only the first token
-        label_all_tokens = True
+            dataset_eval = load_dataset("wikiann", eval_language, trust_remote_code=True)
+            # If True, all tokens of a word will be labeled, otherwise only the first token
+            label_all_tokens = True
 
-        def align_labels_with_tokens(labels, word_ids):
-            new_labels = []
-            current_word = None
-            for word_id in word_ids:
-                if word_id != current_word:
-                    # Start of a new word!
-                    current_word = word_id
-                    label = -100 if word_id is None else labels[word_id]
-                    new_labels.append(label)
-                elif word_id is None:
-                    # Special token
-                    new_labels.append(-100)
+            def align_labels_with_tokens(labels, word_ids):
+                new_labels = []
+                current_word = None
+                for word_id in word_ids:
+                    if word_id != current_word:
+                        # Start of a new word!
+                        current_word = word_id
+                        label = -100 if word_id is None else labels[word_id]
+                        new_labels.append(label)
+                    elif word_id is None:
+                        # Special token
+                        new_labels.append(-100)
+                    else:
+                        # Same word as previous token
+                        label = labels[word_id]
+                        # If the label is B-XXX we change it to I-XXX
+                        if label % 2 == 1:
+                            label += 1
+                        new_labels.append(label)
+
+                return new_labels
+
+            # Batch encoding function for NER
+            def tokenize_and_align_labels(examples):
+                tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+                all_labels = examples["ner_tags"]
+                new_labels = []
+                for i, labels in enumerate(all_labels):
+                    word_ids = tokenized_inputs.word_ids(i)
+                    new_labels.append(align_labels_with_tokens(labels, word_ids))
+
+                tokenized_inputs["labels"] = new_labels
+                return tokenized_inputs
+
+            # Load and preprocess WikiANN
+
+            tokenized_datasets = dataset_eval.map(
+                tokenize_and_align_labels,
+                batched=True,
+                remove_columns=dataset_eval["train"].column_names,
+            )
+
+            # dataset_en is now ready to be used with adapters for cross-lingual transfer
+
+            ner_feature = dataset_eval["train"].features["ner_tags"]
+
+            label_names = ner_feature.feature.names
+            words = dataset_eval["train"][0]["tokens"]
+            labels = dataset_eval["train"][0]["ner_tags"]
+            id2label = {i: label for i, label in enumerate(label_names)}
+            label2id = {v: k for k, v in id2label.items()}
+
+            def compute_metrics(eval_preds):
+                logits, labels = eval_preds
+                predictions = np.argmax(logits, axis=-1)
+
+                # Remove ignored index (special tokens) and convert to labels
+                true_labels = [[label_names[l] for l in label if l != -100] for label in labels]
+                true_predictions = [
+                    [label_names[p] for (p, l) in zip(prediction, label) if l != -100]
+                    for prediction, label in zip(predictions, labels)
+                ]
+                all_metrics = metric.compute(predictions=true_predictions, references=true_labels)
+                return {
+                    "precision": all_metrics["overall_precision"],
+                    "recall": all_metrics["overall_recall"],
+                    "f1": all_metrics["overall_f1"],
+                    "accuracy": all_metrics["overall_accuracy"],
+                }
+
+            def eval(model, name):
+                # we load in the task adapter
+                if not name == "ner":
+                    model.active_adapters = Stack(name, "ner")
                 else:
-                    # Same word as previous token
-                    label = labels[word_id]
-                    # If the label is B-XXX we change it to I-XXX
-                    if label % 2 == 1:
-                        label += 1
-                    new_labels.append(label)
+                    model.active_adapters = name
 
-            return new_labels
+                eval_trainer = AdapterTrainer(
+                    model=model,
+                    args=TrainingArguments(
+                        output_dir="./eval_output",
+                        remove_unused_columns=False,
+                    ),
+                    data_collator=data_collator,
+                    eval_dataset=tokenized_datasets["validation"],
+                    compute_metrics=compute_metrics,
+                )
+                ev = eval_trainer.evaluate()
+                print(f"Evaluation results for {name}:")
+                print(ev)
+                # we empty the cache and model
+                model.cpu()
+                del model
+                del eval_trainer
+                if torch.cuda.is_available():
+                    torch.cuda.empty_cache()
+                gc.collect()
 
-        # Batch encoding function for NER
-        def tokenize_and_align_labels(examples):
-            tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
-            all_labels = examples["ner_tags"]
-            new_labels = []
-            for i, labels in enumerate(all_labels):
-                word_ids = tokenized_inputs.word_ids(i)
-                new_labels.append(align_labels_with_tokens(labels, word_ids))
+                return ev
 
-            tokenized_inputs["labels"] = new_labels
-            return tokenized_inputs
-
-        # Load and preprocess WikiANN
-
-        tokenized_datasets = dataset_eval.map(
-            tokenize_and_align_labels,
-            batched=True,
-            remove_columns=dataset_eval["train"].column_names,
-        )
-
-        # dataset_en is now ready to be used with adapters for cross-lingual transfer
-
-        ner_feature = dataset_eval["train"].features["ner_tags"]
-
-        label_names = ner_feature.feature.names
-        words = dataset_eval["train"][0]["tokens"]
-        labels = dataset_eval["train"][0]["ner_tags"]
-        id2label = {i: label for i, label in enumerate(label_names)}
-        label2id = {v: k for k, v in id2label.items()}
-
-        def compute_metrics(eval_preds):
-            logits, labels = eval_preds
-            predictions = np.argmax(logits, axis=-1)
-
-            # Remove ignored index (special tokens) and convert to labels
-            true_labels = [[label_names[l] for l in label if l != -100] for label in labels]
-            true_predictions = [
-                [label_names[p] for (p, l) in zip(prediction, label) if l != -100]
-                for prediction, label in zip(predictions, labels)
-            ]
-            all_metrics = metric.compute(predictions=true_predictions, references=true_labels)
-            return {
-                "precision": all_metrics["overall_precision"],
-                "recall": all_metrics["overall_recall"],
-                "f1": all_metrics["overall_f1"],
-                "accuracy": all_metrics["overall_accuracy"],
-            }
-
-        def eval(model, name):
-            # we load in the task adapter
-            if not name == "ner":
-                model.active_adapters = Stack(name, "ner")
+            # we check if the adapter has already been created before
+            if os.path.exists(f"./trained_adapters/typological/{eval_language}"):
+                print("Adapter already exists, loading instead")
+                model.load_adapter(
+                    f"./trained_adapters/typological/{eval_language}", load_as="reconstructed_" + eval_language
+                )
             else:
-                model.active_adapters = name
+                target_glot = ld.get(eval_language, tag_type=TagType.BCP_47_CODE).glottocode
+                weights = typological_approximation(target_glot, get_glots(to_load))
 
-            eval_trainer = AdapterTrainer(
-                model=model,
-                args=TrainingArguments(
-                    output_dir="./eval_output",
-                    remove_unused_columns=False,
-                ),
-                data_collator=data_collator,
-                eval_dataset=tokenized_datasets["validation"],
-                compute_metrics=compute_metrics,
-            )
-            ev = eval_trainer.evaluate()
-            print(f"Evaluation results for {name}:")
-            print(ev)
-            # we empty the cache and model
-            model.cpu()
-            del model
-            del eval_trainer
-            if torch.cuda.is_available():
-                torch.cuda.empty_cache()
-            gc.collect()
+                merge_loaded_adapters(
+                    model, merge_adapter_name=f"reconstructed_{eval_language}", weights=weights, delete_other=False
+                )
+                # save this adapter
+                # check if directory exists first
+                if not os.path.exists(f"./trained_adapters/typological/{eval_language}"):
+                    os.makedirs(f"./trained_adapters/typological/{eval_language}")
+                model.save_adapter(f"./trained_adapters/typological/{eval_language}", "reconstructed_" + eval_language)
 
-            return ev
+            model.load_adapter("./trained_adapters/ner", load_as="ner")
 
-        # we check if the adapter has already been created before
-        if os.path.exists(f"./trained_adapters/typological/{eval_language}"):
-            print("Adapter already exists, loading instead")
-            model.load_adapter(
-                f"./trained_adapters/typological/{eval_language}", load_as="reconstructed_" + eval_language
-            )
-        else:
-            target_glot = ld.get(eval_language, tag_type=TagType.BCP_47_CODE).glottocode
-            weights = typological_approximation(target_glot, get_glots(to_load))
+            evaluations = {}
+            evaluations["reconstructed_" + eval_language] = eval(model, f"reconstructed_{eval_language}")
 
-            merge_loaded_adapters(
-                model, merge_adapter_name=f"reconstructed_{eval_language}", weights=weights, delete_other=False
-            )
-            # save this adapter
-            # check if directory exists first
-            if not os.path.exists(f"./trained_adapters/typological/{eval_language}"):
-                os.makedirs(f"./trained_adapters/typological/{eval_language}")
-            model.save_adapter(f"./trained_adapters/typological/{eval_language}", "reconstructed_" + eval_language)
+            # we calculate a baseline (just ner adapter)
+            evaluations["baseline_ner"] = eval(model, "ner")
 
-        model.load_adapter("./trained_adapters/ner", load_as="ner")
+            # we calculate a baseline (just average over all adapter)
+            # we load the mono/huge_avg_adapter for this
+            model.load_adapter("./trained_adapters/typological/huge_avg_adapter", load_as="huge_avg_adapter")
+            evaluations["baseline_avg_adapter"] = eval(model, "huge_avg_adapter")
+            # we delete the added adapters
+            model.delete_adapter("huge_avg_adapter")
+            model.delete_adapter("ner")
+            model.delete_adapter("reconstructed_" + eval_language)
 
-        evaluations = {}
-        evaluations["reconstructed_" + eval_language] = eval(model, f"reconstructed_{eval_language}")
-
-        # we calculate a baseline (just ner adapter)
-        evaluations["baseline_ner"] = eval(model, "ner")
-
-        # we calculate a baseline (just average over all adapter)
-        # we load the mono/huge_avg_adapter for this
-        model.load_adapter("./trained_adapters/typological/huge_avg_adapter", load_as="huge_avg_adapter")
-        evaluations["baseline_avg_adapter"] = eval(model, "huge_avg_adapter")
-        # we delete the added adapters
-        model.delete_adapter("huge_avg_adapter")
-        model.delete_adapter("ner")
-        model.delete_adapter("reconstructed_" + eval_language)
-
-        # we save this
-        with open(f"./trained_adapters/typological/{eval_language}/eval.json", "w") as f:
-            json.dump(evaluations, f, indent=4)
-            print("Saved evaluations to file")
-
+            # we save this
+            with open(f"./trained_adapters/typological/{eval_language}/eval.json", "w") as f:
+                json.dump(evaluations, f, indent=4)
+                print("Saved evaluations to file")
+        except RuntimeError:
+            print("RuntimeError, skipping this language")
+            continue
+        except IndexError:
+            print("IndexError, skipping this language")
+            continue
 
 if __name__ == "__main__":
     job_name = "unseen_eval_random_debug"
