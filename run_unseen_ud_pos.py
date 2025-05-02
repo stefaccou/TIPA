c6865760ef0ce4f60a37b72686f8c77b5743cb4f
Stef Accou <stef.accou@student.kuleuven.be>
2025-05-02 22:31:11 +0200

    fixes on limit customization

--- run_unseen_ud_pos.py
+++ run_unseen_ud_pos.py
@@ -424,7 +424,7 @@ if __name__ == "__main__":
             "cpus_per_gpu": 16,
             "gpus_per_node": 1,
             "mail_type": "BEGIN,END,FAIL",
-            "mail_user": "stef.accou@student.kuleuven.be",
+            "mail_user": f"{'' if partition.endswith('debug') else 'stef.accou@student.kuleuven.be'}",
         },
     }
 
