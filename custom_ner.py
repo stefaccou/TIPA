c5ab407a6f70785013848f18e178f01c997801b4
Stef Accou <stef.accou@student.kuleuven.be>
2025-05-05 22:11:39 +0200

    ner fixes

--- custom_ner.py
+++ custom_ner.py
@@ -140,7 +140,7 @@ def main(submit_arguments):
         save_steps=25000,
         num_train_epochs=100,
         weight_decay=0.01,
-        overwrite_output_dir=True,
+        overwrite_output_dir=False,
         # The next line is important to ensure the dataset labels are properly passed to the model
         remove_unused_columns=False,
     )
