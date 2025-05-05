bdaa650d7320781e69f25e9a5919277b436daba8
Stef Accou <stef.accou@student.kuleuven.be>
2025-05-05 22:41:42 +0200

    training better adapters

--- custom_copa.py
+++ custom_copa.py
@@ -4,12 +4,37 @@ import os
 from custom_submission_utils import find_master, update_submission_log
 
 
-def main():
-    from datasets import load_dataset, concatenate_datasets
-    from transformers import TrainingArguments, AutoTokenizer, AutoConfig, EvalPrediction
-    from adapters import AdapterTrainer, AutoAdapterModel, AdapterConfig
+def main(submit_arguments):
+    from datasets import load_dataset
+    from dataclasses import dataclass, field
+    from transformers import TrainingArguments, AutoTokenizer, AutoConfig, EvalPrediction, HfArgumentParser
+    from adapters import AdapterTrainer, AutoAdapterModel
     from adapters.composition import Stack
     import numpy as np
+
+    @dataclass
+    class DataTrainingArguments:
+        """
+        Arguments pertaining to what data we are going to input our model for training and eval.
+        """
+
+        output_dir: str = field(
+            default=None,
+            metadata={"help": ("Directory for model checkpoints and saving")},
+        )
+
+    parser = HfArgumentParser(DataTrainingArguments)
+    sys.argv = ""
+    if len(submit_arguments) == 1 and submit_arguments[0].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        data_args = parser.parse_json_file(json_file=os.path.abspath(submit_arguments[0]))
+    else:
+        print("calling parser")
+        # add a comma to refer to first part of tuple output
+        (data_args,) = parser.parse_args_into_dataclasses(submit_arguments)
+
+    print("passed args: ", data_args)
 
     tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
     raw_datasets = load_dataset("super_glue", "copa")
@@ -60,34 +85,30 @@ def main():
     # (Optionally) load language adapters if needed
     model.load_adapter("./trained_adapters/en", load_as="en")
     model.add_adapter("copa")
-    model.add_multiple_choice_head(
-        "copa", num_choices=2, id2label={0: "choice1", 1: "choice2"}
-    )
+    model.add_multiple_choice_head("copa", num_choices=2, id2label={0: "choice1", 1: "choice2"})
     model.train_adapter(["copa"])
     model.active_adapters = Stack("en", "copa")
     print(model.active_adapters)
 
     training_args = TrainingArguments(
-        output_dir="./trained_adapters/custom_copa_adapter",
-        #eval_strategy="epoch",
-        learning_rate=1e-5,
-        per_device_train_batch_size=32,
-        #per_device_eval_batch_size=32,
-        #num_train_epochs=3,
-        max_steps=10000,
+        output_dir=data_args.output_dir,
+        eval_strategy="epoch",
+        learning_rate=1e-4,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=100,
         weight_decay=0.01,
-        logging_dir="./logs",
-        logging_steps=250,
+        save_steps=25000,
         overwrite_output_dir=True,
         # The next line is important to ensure the dataset labels are properly passed to the model
         remove_unused_columns=False,
     )
-    train_dataset = concatenate_datasets([tokenized_datasets["train"], tokenized_datasets["validation"]])
+
     trainer = AdapterTrainer(
         model=model,
         args=training_args,
-        train_dataset=train_dataset,
-        #eval_dataset=tokenized_datasets["test"],
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["validation"],
         compute_metrics=compute_accuracy,
     )
     trainer.train()
@@ -95,9 +116,8 @@ def main():
 
 
 if __name__ == "__main__":
-    # we want just the one argument as a string here
+    job_name = "better_copa_adapter"
 
-    job_name = "training_custom_copa_adapter"
     master_dir = find_master()
 
     # Set the experiment folder as a subdirectory of 'Master_thesis'
@@ -106,24 +126,29 @@ if __name__ == "__main__":
     run_count = update_submission_log(experiments_dir, job_name)
     experiments_dir = experiments_dir / job_name / f"{run_count:03d}"
     experiments_dir.mkdir(parents=True, exist_ok=True)  # Create if it doesn't exist
+    partition = "gpu_p100"
     parameters = {
-        "slurm_partition": "gpu_p100",
-        "slurm_time": "02:00:00",
+        "slurm_partition": partition,
+        # "slurm_time": "03:00:00",
+        "slurm_time": f"{'01:00:00' if partition.endswith('debug') else '5:00:00'}",
         "slurm_job_name": job_name,
         "slurm_additional_parameters": {
-            "clusters": "genius",
+            "clusters": f"{'genius' if partition.startswith('gpu_p100') else 'wice'}",
             "account": os.environ["ACCOUNT_INFO"],  # replace with your account
             "nodes": 1,
             "cpus_per_gpu": 16,
             "gpus_per_node": 1,
-            "mail_type": "END",
-            "mail_user": "",
+            "mail_type": "BEGIN,END,FAIL",
+            "mail_user": f"{'' if partition.endswith('debug') else 'stef.accou@student.kuleuven.be'}",
         },
     }
 
     # Initialize the Submitit executor with the new experiments_dir
     executor = submitit.AutoExecutor(folder=str(experiments_dir))
     executor.update_parameters(**parameters)
-    submitit_input = sys.argv[1:] if len(sys.argv) > 1 else "No input passed"
-    job = executor.submit(main)
-    print("eval submitted")
+
+    job_input = sys.argv[1:] if len(sys.argv) > 1 else "default text"
+
+    job = executor.submit(main, job_input)
+    # job = executor.submit(main)
+    print("job submitted")
--- custom_ud.py
+++ custom_ud.py
@@ -4,14 +4,44 @@ import os
 from custom_submission_utils import find_master, update_submission_log
 
 
-def main():
+def main(submit_arguments):
     from datasets import load_dataset
-    from transformers import AutoModelForTokenClassification, TrainingArguments, XLMRobertaTokenizerFast
+    from dataclasses import dataclass, field
+    from transformers import (
+        AutoModelForTokenClassification,
+        TrainingArguments,
+        XLMRobertaTokenizerFast,
+        HfArgumentParser,
+    )
     from adapters import AdapterTrainer, init
     from adapters.composition import Stack
     from transformers import DataCollatorForTokenClassification
     import numpy as np
     from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+
+    @dataclass
+    class DataTrainingArguments:
+        """
+        Arguments pertaining to what data we are going to input our model for training and eval.
+        """
+
+        output_dir: str = field(
+            default=None,
+            metadata={"help": ("Directory for model checkpoints and saving")},
+        )
+
+    parser = HfArgumentParser(DataTrainingArguments)
+    sys.argv = ""
+    if len(submit_arguments) == 1 and submit_arguments[0].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        data_args = parser.parse_json_file(json_file=os.path.abspath(submit_arguments[0]))
+    else:
+        print("calling parser")
+        # add a comma to refer to first part of tuple output
+        (data_args,) = parser.parse_args_into_dataclasses(submit_arguments)
+
+    print("passed args: ", data_args)
 
     tokenizer = XLMRobertaTokenizerFast.from_pretrained("xlm-roberta-base")
     raw_datasets = load_dataset("universal_dependencies", "en_ewt", trust_remote_code=True)
@@ -108,15 +138,14 @@ def main():
     print(model.active_adapters)
 
     training_args = TrainingArguments(
-        output_dir="./trained_adapters/custom_ud_pos_adapter",
+        output_dir=data_args.output_dir,
         evaluation_strategy="epoch",
         learning_rate=1e-4,
-        per_device_train_batch_size=32,
-        per_device_eval_batch_size=32,
-        num_train_epochs=3,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=100,
         weight_decay=0.01,
-        logging_dir="./logs",
-        logging_steps=250,
+        save_steps=25000,
         overwrite_output_dir=True,
         # The next line is important to ensure the dataset labels are properly passed to the model
         remove_unused_columns=False,
@@ -136,9 +165,8 @@ def main():
 
 
 if __name__ == "__main__":
-    # we want just the one argument as a string here
+    job_name = "better_ud_adapter"
 
-    job_name = "training_custom_ud_pos_adapter"
     master_dir = find_master()
 
     # Set the experiment folder as a subdirectory of 'Master_thesis'
@@ -147,24 +175,29 @@ if __name__ == "__main__":
     run_count = update_submission_log(experiments_dir, job_name)
     experiments_dir = experiments_dir / job_name / f"{run_count:03d}"
     experiments_dir.mkdir(parents=True, exist_ok=True)  # Create if it doesn't exist
+    partition = "gpu_p100"
     parameters = {
-        "slurm_partition": "gpu_p100_debug",
-        "slurm_time": "00:10:00",
+        "slurm_partition": partition,
+        # "slurm_time": "03:00:00",
+        "slurm_time": f"{'01:00:00' if partition.endswith('debug') else '5:00:00'}",
         "slurm_job_name": job_name,
         "slurm_additional_parameters": {
-            "clusters": "genius",
+            "clusters": f"{'genius' if partition.startswith('gpu_p100') else 'wice'}",
             "account": os.environ["ACCOUNT_INFO"],  # replace with your account
             "nodes": 1,
             "cpus_per_gpu": 16,
             "gpus_per_node": 1,
-            "mail_type": "END",
-            "mail_user": "",
+            "mail_type": "BEGIN,END,FAIL",
+            "mail_user": f"{'' if partition.endswith('debug') else 'stef.accou@student.kuleuven.be'}",
         },
     }
 
     # Initialize the Submitit executor with the new experiments_dir
     executor = submitit.AutoExecutor(folder=str(experiments_dir))
     executor.update_parameters(**parameters)
-    submitit_input = sys.argv[1:] if len(sys.argv) > 1 else "No input passed"
-    job = executor.submit(main)
+
+    job_input = sys.argv[1:] if len(sys.argv) > 1 else "default text"
+
+    job = executor.submit(main, job_input)
+    # job = executor.submit(main)
     print("job submitted")
