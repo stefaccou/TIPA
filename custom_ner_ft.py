478ab8b55a71db69817269f33f04bf6ab65f2f89
Stef Accou <stef.accou@student.kuleuven.be>
2025-05-16 13:41:23 +0200

    ner finetune untill convergence

--- custom_ner_ft.py
+++ custom_ner_ft.py
@@ -147,7 +147,7 @@ def main(submit_arguments):
 
 
 if __name__ == "__main__":
-    debug = True
+    debug = False
     job_name = debug * "debug_" + "finetune_ner"
 
     master_dir = find_master()
