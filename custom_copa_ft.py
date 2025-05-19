7705fb0b46be9a3e5b486099fa5c4decccab3f7a
Stef Accou <stef.accou@student.kuleuven.be>
2025-05-19 22:33:12 +0200

    finetuning copa

--- custom_copa_ft.py
+++ custom_copa_ft.py
@@ -122,7 +122,7 @@ def main(submit_arguments):
 
 if __name__ == "__main__":
     debug = False
-    job_name = debug * "debug_" + "finetune_copa"
+    job_name = debug * "debug_" + "convergence_finetune_copa"
 
     master_dir = find_master()
 
