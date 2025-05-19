20e5c5d175b9f64bd9bd02e50b41b4d24fa5544d
Stef Accou <stef.accou@student.kuleuven.be>
2025-05-19 19:58:48 +0200

    debugging qa finetune

--- custom_qa_ft.py
+++ custom_qa_ft.py
@@ -195,7 +195,7 @@ def main(submit_arguments):
 
 
 if __name__ == "__main__":
-    debug = True
+    debug = False
     job_name = "debug_" * debug + "convergence_finetune_qa"
 
     master_dir = find_master()
