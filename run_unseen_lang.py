c4dc0de67ea5da94c1fd5fe44ee45482a212768d
Stef Accou <stef.accou@student.kuleuven.be>
2025-05-20 19:38:13 +0200

    converged qa adapter eval

--- run_unseen_lang.py
+++ run_unseen_lang.py
@@ -443,7 +443,7 @@ def main(submit_arguments):
 
 
 if __name__ == "__main__":
-    debug = False
+    debug = True
     job_name = debug * "debug_" + "unseen_task"
 
     master_dir = find_master()
