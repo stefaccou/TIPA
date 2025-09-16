7b80e030421fdd3ecfb33e44f7b9eb3fd637e5d6
Stef Accou <stef.accou@student.kuleuven.be>
2025-09-16 15:59:54 +0200

    improve clm config

--- run_clm.py
+++ run_clm.py
@@ -776,7 +776,7 @@ def main(submit_arguments):
 if __name__ == "__main__":
     job_name = "clm_adapter"
     debug = True
-    partition = "p100"
+    partition = "a100"
     time = "04:00:00"
 
     master_dir = find_master()
